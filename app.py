bb9f7e4ab003c795c9810a352bedf289dd76e372
VinATU <l00179000@atu.ie>
2023-11-11 22:07:17 +0000

    Changed docker password variable

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World to DevOps Project!'
+    return 'Hello, World to DevOps Project app!'
 
 @app.route('/hello', methods=['GET', 'POST'])
 def hello():
